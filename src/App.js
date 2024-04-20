c94707c981548969fa4860b7fc88d4683cd56f98
gitlab 이름 <nwy98@naver.com>
2024-04-20 15:54:30 +0900

    [Init] 폴더, 페이지 분리

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Onboarding from "./pages/Onboarding.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/home" element={<Home />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
