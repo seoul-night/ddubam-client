d74ae4efa3616b839c1455e80285b0f4cb149305
gitlab 이름 <nwy98@naver.com>
2024-04-27 01:42:47 +0900

    [Design] Home 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullseye } from "@fortawesome/free-solid-svg-icons";
 import chimps from "../assets/chimps.webp";
 import Footer from "../components/Footer";
+import homeback from "../assets/homeback.png";
+import logo from "../assets/logo.png";
+import location from "../assets/icons/location.png";
+import homebtn1 from "../assets/homebtn1.png";
+import homebtn2 from "../assets/homebtn2.png";
 
 const HomeWrapper = styled.div`
+  z-index: -100;
   min-height: 100vh;
   padding-bottom: 90px; // Footer 높이를 고려한 하단 여백
-  background: linear-gradient(#38384e, #21212f);
+  background: #1c1c26;
   overflow: auto; // 스크롤을 명시적으로 허용
   position: relative;
   padding-bottom: 200px;
 `;
 
 const Head = styled.div`
+  position: fixed;
+  background-color: #3f3fc4;
+  top: 0px;
   display: flex;
+  width: 100%;
   align-items: center;
+  box-sizing: border-box;
   justify-content: space-between;
   padding: 25px;
   h2 {
@@ -90,6 +100,7 @@ const Box = styled.div`
   box-sizing: border-box;
   border-radius: 10px;
   margin-bottom: 10px;
+  position: relative;
 `;
 const SubText = styled.p`
   font-size: 12px;
@@ -100,15 +111,42 @@ const SubText = styled.p`
 const Home = () => {
   return (
     <HomeWrapper className="Home">
-      <Head>
-        <diV>
-          <h2>밤안갱 로고</h2>
-        </diV>
-        <div>
-          <Region>
-            <FontAwesomeIcon icon={faBullseye} />
-          </Region>
-          <span>서울 송파구</span>
+      <div>
+        <img
+          className="Home"
+          src={homeback}
+          style={{
+            position: "fixed",
+            zIndex: "-1",
+            width: "100%",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        />
+      </div>
+      <Head className="Home">
+        <div
+          style={{
+            justifyContent: "space-between",
+
+            display: "flex",
+            width: "100%",
+          }}
+        >
+          <img src={logo} />
+          <div
+            style={{
+              position: "absolute",
+              right: "0",
+              display: "flex",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            <img src={location} style={{ marginRight: "3px" }} />
+            <span>서울 송파구</span>
+          </div>
         </div>
       </Head>
       <UserWrap>
@@ -133,10 +171,18 @@ const Home = () => {
           <Box style={{ width: "154px", backgroundColor: "#5F53E8" }}>
             <Text>현 위치 기반</Text>
             <SubText>내 주변에서 산책해요</SubText>
+            <img
+              src={homebtn1}
+              style={{ position: "absolute", bottom: "15px", right: "15px" }}
+            />
           </Box>
           <Box style={{ width: "154px", backgroundColor: "#467FEE" }}>
             <Text>인기있는</Text>
             <SubText>사람들이 많이 다녀요</SubText>
+            <img
+              src={homebtn2}
+              style={{ position: "absolute", bottom: "15px", right: "15px" }}
+            />
           </Box>
         </div>
       </GoWalk>
