1fecd5382b569ebc380fe13902688368d92dc574
gitlab 이름 <nwy98@naver.com>
2024-04-28 01:14:13 +0900

    [Feat] 404페이지

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NearbyPath from "./pages/NearbyPath.jsx";
 import FinishedPath from "./pages/FinishedPath.jsx";
 import LikedPath from "./pages/LikedPath.jsx";
 import PopularPath from "./pages/PopularPath.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   // APp.js 렌더링시 뷰포트 높이 계산 함수
@@ -28,6 +29,7 @@ function App() {
         <Route path="/finished" element={<FinishedPath />} />
         <Route path="/liked" element={<LikedPath />} />
         <Route path="/popular" element={<PopularPath />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
