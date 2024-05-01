11dee38babd31fb94b06fa4a675eea4a9b30bf8a
nwy98 <97599088+norunaru@users.noreply.github.com>
2024-05-01 15:25:40 +0900

    [Env] recoil

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import { createGlobalStyle } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       {/* <GlobalStyle /> */}
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
