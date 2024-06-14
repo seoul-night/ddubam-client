e7fb79a9dec3db49b598f06e27157722123ab0d4
jaejunlee11 <sktks11@naver.com>
2024-06-14 18:28:19 +0900

    release-v2.3

---
+++ build/service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/js/main.chunk.js",
+  "/static/js/vendors~main.chunk.js",
+  "/static/css/main.chunk.css",
+  "/manifest.json",
+  "/favicon.ico",
+  "/logo192.png",
+  "/logo512.png",
+  "/16.png",
+  "/32.png",
+  "/48.png",
+  "/72.png",
+  "/96.png",
+  "/144.png",
+  "/192.png",
+  "/512.png",
+];
+
+// 설치 이벤트
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 요청 가로채기
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
